a2f2953a1d54ecc75fb624d156fcfa6543418547
Tran Anh Kiet <kiettran0504@gmail.com>
2025-06-10 09:25:01 +0700

    setup v2

--- vietnova/src/App.tsx
+++ vietnova/src/App.tsx
@@ -1,27 +1,15 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from 'components/pages/Login';
-import PlayerProfile from 'components/pages/PlayerProfile';
-import TeamProfile from 'components/pages/TeamProfile';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import ListPlayer from '@/organisms/ListPlayer';
-import ListTeam from '@/organisms/ListTeam';
-
-import Homepage from './components/pages/Homepage';
-import Landingpage from './components/pages/Ladingpage';
+import Homepage from './components/pages/Homepage.tsx';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/' element={<Landingpage />} />
-        <Route path='/home-page' element={<Homepage />} />
-        <Route path='/teams' element={<ListTeam />} />
-        <Route path='/team/:id' element={<TeamProfile />} />
-        <Route path='/players' element={<ListPlayer />} />
-        <Route path='/player/:id' element={<PlayerProfile />} />
+        <Route path="/" element={<Homepage />} />
       </Routes>
     </Router>
   );
 }
+
 export default App;
