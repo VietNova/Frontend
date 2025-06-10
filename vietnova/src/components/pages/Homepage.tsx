32780f560a15ce4d07603c53eb9889bdb4d5f897
Tran Anh Kiet <kiettran0504@gmail.com>
2025-06-10 11:40:27 +0700

    delete

--- vietnova/src/components/pages/Homepage.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from 'react';
-
-import HomepageTemp from '../templates/HompageTemp';
-
-const Hompage: FC = () => <HomepageTemp></HomepageTemp>;
-
-export default Hompage;
--- vietnova/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client';
-
-import App from './App.js';
-
-import './index.css';
-
-const rootElement = document.getElementById('root') as HTMLElement;
-
-ReactDOM.createRoot(rootElement).render(<App />);
